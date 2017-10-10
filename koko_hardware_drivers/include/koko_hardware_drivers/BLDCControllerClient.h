8144c411d8a931974c967ea8c8bd3e1f3af3a16b
Eric Zhou <ericfzhou@berkeley.edu>
2017-10-09 19:34:06 -0700

    correct cpp driver files

--- koko_hardware_drivers/include/koko_hardware_drivers/BLDCControllerClient.h
+++ koko_hardware_drivers/include/koko_hardware_drivers/BLDCControllerClient.h
@@ -17,6 +17,11 @@ class BLDCControllerClient {
     bytebuf_t readResponse(uint8_t server_id, uint8_t func_code, unsigned int num_tries, unsigned int try_interval);
     bytebuf_t readRegisters(uint8_t server_id, uint16_t start_addr, uint8_t count);
     bytebuf_t doTransaction(uint8_t server_id, uint8_t func_code, bytebuf_t& data);
+    uint16_t getEncoder(uint8_t id);
+    bool leaveBootloader(uint8_t server_id, unsigned int jump_addr);
+    bool setDuty(uint8_t id, float value);
     BLDCControllerClient(std::string port, unsigned int baud, serial::Timeout to);
 };
+
+void sleep(unsigned int milliseconds);
 #endif /* BLDCCONTROLLERCLIENT_H */
