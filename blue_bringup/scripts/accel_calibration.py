6323d04ee99753ddcfd4e9c01b62f43cb9b0ee1f
Philipp <phil80301@berkeley.edu>
2018-11-24 14:25:40 -0800

    everything written need to debug

--- blue_bringup/scripts/accel_calibration.py
+++ blue_bringup/scripts/accel_calibration.py
@@ -12,12 +12,18 @@ from sensor_msgs.msg import JointState
 import PyKDL as kdl
 import kdl_parser_py.urdf as kdl_parser
 from blue_msgs.srv import JointStartupCalibration
+from blue_msgs.msg import GravityVectorArray
+from message_filters import TimeSynchronizer, Subscriber, ApproximateTimeSynchronizer
+import numpy as np
+from urdf_parser_py import urdf
 
 # http://docs.ros.org/indigo/api/message_filters/html/python/#message_filters.TimeSynchronizer
 # synch callbacks with this
 
-def measurement_likelihood(vec_part, vec_meas):
-    return np.linalg.norm(vec_part.dot(vec_meas))
+def measurement_likelihood(v_part, v_meas):
+    part = np.array([v_part.x(), v_part.y(), v_part.z()])
+    meas = np.array([v_meas.x, v_meas.y, v_meas.z])
+    return np.abs(part.dot(meas))
 
 class ParticleGroup:
     def __init__(self, num_joints, num_particles, joint_min, joint_max, fk):
@@ -26,24 +32,42 @@ class ParticleGroup:
         self.jmin = joint_min
         self.jmax = joint_max
         self.fk_solver = fk
-        self.particle_offsets = np.uniform.random(self.jmin, self.jmax, (num_p, num_j))
+        # uniformly initalize potential positions from given max and min
+        self.particle_offsets = np.random.uniform(self.jmin, self.jmax, (self.num_p, self.num_j))
+        rospy.logerr(self.particle_offsets)
 
     def update(self, joints, accel_1, accel_2):
-        # TODO figure out accel message type
         ml_array = []
+        kdl_joints = kdl.JntArray(self.num_j)
+        frame = kdl.Frame()
+
+        # for each particle (row joint offset) check the likelyhood given the measurement
         for p in self.particle_offsets:
-            frame = kdl.Frame()
-            self.fk_solver.JntToCart(joints + p, frame)
+            # get the joints angle with offset for the given particle
+            corrected_joints = np.array(joints) + p
+            for j in range(self.num_j):
+                kdl_joints[j] = corrected_joints[j]
+            self.fk_solver.JntToCart(kdl_joints, frame)
+            # find the frame transformation
+
             grav_prev = kdl.Vector()
-            grav_prev.data = accel_1
+            grav_prev.x(accel_1.x)
+            grav_prev.y(accel_1.y)
+            grav_prev.z(accel_1.z)
+
+            # transform the gravity vector by the given frame
             grav_post = frame * grav_prev
+
+            # append the measurement likelyhood
             ml = measurement_likelihood(grav_post, accel_2)
             ml_array.append(ml)
 
 
+        # normalize measurement likelyhood and resample particles
         alpha = np.sum(ml_array)
         ml_array = np.array(ml_array) / alpha
         cdf = np.cumsum(ml_array)
+
         # TODO verify that this is done correctly
         self.particle_offsets = np.array([self.particle_offsets[np.argwhere(cdf>np.random.uniform())[0,0],:] for i in range(self.num_p)])
 
@@ -58,9 +82,12 @@ class BlueRobotFilter:
     def _setup(self):
         # load in ros parameters
         self.baselink = rospy.get_param("blue_hardware/baselink")
-        self.endlink = rospy.get_param("blue_harware/endlink")
-        flag, self.tree = kdl_parser.treeFromParam("/robot_description")
+        self.endlink = rospy.get_param("blue_hardware/endlink")
+        robot_description = rospy.get_param("/robot_description")
+        flag, self.tree = kdl_parser.treeFromString(robot_description)
+
         # build kinematic chain and fk and jacobian solvers
+        self.robot_urdf = urdf.Robot.from_xml_string(robot_description)
         chain_ee = self.tree.getChain(self.baselink, self.endlink)
 
         # building robot joint state
@@ -68,33 +95,46 @@ class BlueRobotFilter:
 
         # other joint information
         self.joint_names = rospy.get_param("blue_hardware/joint_names")
-        self.accel_links = rospy.get_param("blue_hardware/accel_links").insert(0, self.baselink)
+        self.accel_links = rospy.get_param("blue_hardware/accel_links")
+        self.differential_pairs = rospy.get_param("blue_hardware/differential_pairs")
+
+        # load in min and max joints
+        self.jmin = []
+        self.jmax = []
+        for j in range(self.num_joints):
+            joint_limit = self.robot_urdf.joint_map[self.joint_names[j]].limit
+            self.jmin.append(joint_limit.lower)
+            self.jmax.append(joint_limit.upper)
+
+
         self.accel_links.insert(0, self.baselink)
         self.best_estimate = np.zeros(self.num_joints)
         self.probability = 0
 
         # TODO make a rosparam
-        particles_per_joints = 100
+        particles_per_joint = 100
 
         ppj = particles_per_joint
         p_groups = []
         joints_per_group = []
         i = 0
         g = 0
-        while i < num_joints:
-            if i in differential_pairs:
-                chain = self.tree.getChain(self.accel_links[g], self.accel_links[g+1])
-                fk = kdl.ChainFkSolverPos_recursive(chain)
-                pg = ParticleGroup(2, ppj, [jmin[i], jmin[i+1]], [jmax[i], jmax[i+1]], fk)
 
+        # build chain and filter between each gravity vector
+        while i < self.num_joints:
+            # get chain between two accel_links and build respective fk solver
+            chain = self.tree.getChain(self.accel_links[g], self.accel_links[g+1])
+            rospy.logerr("pair: " + self.accel_links[g] +", " + self.accel_links[g+1])
+            fk = kdl.ChainFkSolverPos_recursive(chain)
+
+            # build particle groups
+            if i in self.differential_pairs:
+                pg = ParticleGroup(2, ppj, [self.jmin[i], self.jmin[i+1]], [self.jmax[i], self.jmax[i+1]], fk)
                 p_groups.append(pg)
                 joints_per_group.append(2)
                 i = i + 1
             else:
-                chain = self.tree.getChain(self.accel_links[g], self.accel_links[g+1])
-                fk = kdl.ChainFkSolverPos_recursive(chain)
-                pg = ParticleGroup(1, ppj, [jmin[i]], [jmax[i]], fk)
-
+                pg = ParticleGroup(1, ppj, [self.jmin[i]], [self.jmax[i]], fk)
                 p_groups.append(pg)
                 joints_per_group.append(1)
             i = i + 1
@@ -115,31 +155,41 @@ class BlueRobotFilter:
     def get_best_estimate(self):
         return self.best_estimate
 
-    def update_filter(self, joints, accel):
-        new_joints = self.joints.copy()
+    def update_filter(self, joints_msg, accel_msg):
+
+        # retrive joint angles in correct order from joint state message (comes unordered)
+        new_joint_positions = []
+        for jn in self.joint_names:
+            index = joints_msg.name.index(jn)
+            new_joint_positions.append(joints_msg.position[index])
+
         i = 0
         probabilities = []
         best_estimate = []
         for j, pg in enumerate(self.pg_array):
-            index = joint_msg.name.index(n)
-            self.joints[i] = joint_msg.position[index]
+            # for each particle filter group querry the relevent joint angles
             new_link_joints = []
             for k in range(self.jpg[j]):
-                new_link_joints.append(joint_msg.position[i])
+                new_link_joints.append(new_joint_positions[i])
                 i = i + 1
-            joints, prob = pg.update(new_link_joints, self.accel_links[j], self.accel_links[j+1])
+            # update the filter with new accelerometer values and joint angles
+            joints, prob = pg.update(new_link_joints, accel_msg.vectors[j], accel_msg.vectors[j+1])
             probabilities.append(prob)
             best_estimate.append(joints)
 
-        self.best_estimate = np.array(best_estimate)
+        self.best_estimate = np.array([item for sublist in best_estimate for item in sublist])
         self.probability = np.min(probabilities)
 
+        # rospy.logerr(best_estimate)
+        # rospy.logerr(probabilities)
+
     def calibrate(self):
-        # TODO write this function which uses the synchronized callbacks
-        # rospy.Subscriber("/accel", Brents custom accel, self.update_joints)
-        rospy.Subscriber("/joint_states", JointState, self.update_joints)
-        # synch callbacks with this
-        # sych callback = update filter
+        joint_state_subscriber = Subscriber("/joint_states", JointState)
+        gravity_vector_subscriber = Subscriber("blue_hardware/gravity_vectors", GravityVectorArray)
+        tss = ApproximateTimeSynchronizer([joint_state_subscriber, gravity_vector_subscriber], queue_size=2, slop=0.05)
+
+        # register joint states and gravity vectors callback
+        tss.registerCallback(self.update_filter)
 
 if __name__ == "__main__":
     rospy.init_node("simple_startup_calibration")
@@ -147,7 +197,11 @@ if __name__ == "__main__":
     # building the kinematic chains
     rospy.loginfo("Building Blue Object...")
     blue = BlueRobotFilter(debug=True)
+
+    # start the calibration callback functions
     blue.calibrate()
+
+    # wait until a high probability is reached
     r = rospy.Rate(1.0)
     while blue.get_probability() < 0.9:
         r.sleep();
