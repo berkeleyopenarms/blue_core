a23bdf06dc10467a403de5a65f97ef826320195a
Philipp <phil80301@berkeley.edu>
2018-11-24 17:09:31 -0800

    debuging

--- blue_bringup/scripts/accel_calibration.py
+++ blue_bringup/scripts/accel_calibration.py
@@ -3,6 +3,7 @@
 """This node should run at startup, and sets the initial joint angles to some hardcoded values."""
 
 import rospy
+import pickle
 import sys
 import actionlib
 from std_msgs.msg import Int32
@@ -16,6 +17,7 @@ from blue_msgs.msg import GravityVectorArray
 from message_filters import TimeSynchronizer, Subscriber, ApproximateTimeSynchronizer
 import numpy as np
 from urdf_parser_py import urdf
+from scipy.stats import multivariate_normal
 
 # http://docs.ros.org/indigo/api/message_filters/html/python/#message_filters.TimeSynchronizer
 # synch callbacks with this
@@ -24,6 +26,12 @@ def measurement_likelihood(v_part, v_meas):
     part = np.array([v_part.x(), v_part.y(), v_part.z()])
     meas = np.array([v_meas.x, v_meas.y, v_meas.z])
     return np.abs(part.dot(meas))
+
+# def measurement_likelihood(v_part, v_meas):
+    # part = np.array([v_part.x(), v_part.y(), v_part.z()])
+    # meas = np.array([v_meas.x, v_meas.y, v_meas.z])
+    # ml = multivariate_normal.pdf(part, mean=meas, cov=100.0*np.ones(3))
+    # return ml
 
 class ParticleGroup:
     def __init__(self, num_joints, num_particles, joint_min, joint_max, fk):
@@ -34,10 +42,10 @@ class ParticleGroup:
         self.fk_solver = fk
         # uniformly initalize potential positions from given max and min
         self.particle_offsets = np.random.uniform(self.jmin, self.jmax, (self.num_p, self.num_j))
-        rospy.logerr(self.particle_offsets)
+        # rospy.logerr(self.particle_offsets)
 
     def update(self, joints, accel_1, accel_2):
-        ml_array = []
+        measurement_likelihood_array = []
         kdl_joints = kdl.JntArray(self.num_j)
         frame = kdl.Frame()
 
@@ -60,27 +68,31 @@ class ParticleGroup:
 
             # append the measurement likelyhood
             ml = measurement_likelihood(grav_post, accel_2)
-            ml_array.append(ml)
+            measurement_likelihood_array.append(ml)
 
 
         # normalize measurement likelyhood and resample particles
-        alpha = np.sum(ml_array)
-        ml_array = np.array(ml_array) / alpha
-        cdf = np.cumsum(ml_array)
+        alpha = np.sum(measurement_likelihood_array)
+        measurement_likelihood_array= np.array(measurement_likelihood_array) / alpha
+        cdf = np.cumsum(measurement_likelihood_array)
 
         # TODO verify that this is done correctly
         self.particle_offsets = np.array([self.particle_offsets[np.argwhere(cdf>np.random.uniform())[0,0],:] for i in range(self.num_p)])
 
         # TODO implement roughening
-        if False:
-            pass
-
-        index = np.argmax(ml_array)
-        return self.particle_offsets[index,:], ml_array[index]
+        if True:
+            mean = np.zeros(self.num_j)
+            sigma = 0.0001 * 2.0 * self.num_p**(-1.0/3.0) * np.ones((self.num_j, self.num_j))
+            noise = np.random.multivariate_normal(mean, sigma, self.num_p)
+            self.particle_offsets = self.particle_offsets + noise
+        index = np.argmax(measurement_likelihood_array)
+        return self.particle_offsets[index,:], measurement_likelihood_array[index]
 
 class BlueRobotFilter:
     def _setup(self):
         # load in ros parameters
+        self.gt= np.array(rospy.get_param("blue_hardware/simple_startup_angles"))
+
         self.baselink = rospy.get_param("blue_hardware/baselink")
         self.endlink = rospy.get_param("blue_hardware/endlink")
         robot_description = rospy.get_param("/robot_description")
@@ -97,6 +109,7 @@ class BlueRobotFilter:
         self.joint_names = rospy.get_param("blue_hardware/joint_names")
         self.accel_links = rospy.get_param("blue_hardware/accel_links")
         self.differential_pairs = rospy.get_param("blue_hardware/differential_pairs")
+        rospy.logerr(self.joint_names)
 
         # load in min and max joints
         self.jmin = []
@@ -105,6 +118,8 @@ class BlueRobotFilter:
             joint_limit = self.robot_urdf.joint_map[self.joint_names[j]].limit
             self.jmin.append(joint_limit.lower)
             self.jmax.append(joint_limit.upper)
+        rospy.logerr(self.jmin)
+        rospy.logerr(self.jmax)
 
 
         self.accel_links.insert(0, self.baselink)
@@ -112,7 +127,7 @@ class BlueRobotFilter:
         self.probability = 0
 
         # TODO make a rosparam
-        particles_per_joint = 100
+        particles_per_joint = 200
 
         ppj = particles_per_joint
         p_groups = []
@@ -147,6 +162,7 @@ class BlueRobotFilter:
         self.debug = debug
         if self.debug:
             self.debug_count = 0
+            self.history = []
         self._setup()
 
     def get_probability(self):
@@ -156,7 +172,6 @@ class BlueRobotFilter:
         return self.best_estimate
 
     def update_filter(self, joints_msg, accel_msg):
-
         # retrive joint angles in correct order from joint state message (comes unordered)
         new_joint_positions = []
         for jn in self.joint_names:
@@ -166,6 +181,7 @@ class BlueRobotFilter:
         i = 0
         probabilities = []
         best_estimate = []
+
         for j, pg in enumerate(self.pg_array):
             # for each particle filter group querry the relevent joint angles
             new_link_joints = []
@@ -177,33 +193,56 @@ class BlueRobotFilter:
             probabilities.append(prob)
             best_estimate.append(joints)
 
+        if self.debug:
+            self.debug_count += 1
+            all_particles = []
+            for pg in self.pg_array:
+                all_particles.append(pg.particle_offsets.copy())
+            # rospy.logerr(all_particles)
+            all_particles = np.hstack(all_particles)
+            if self.debug_count&5 == 0:
+                # rospy.logerr(all_particles.shape)
+                rospy.logerr(len(self.history))
+                self.history.append(all_particles)
+                if len(self.history) == 100:
+                    with open('/home/phil/gauss.pickle', 'wb') as handle:
+                            pickle.dump(self.history, handle)
+
         self.best_estimate = np.array([item for sublist in best_estimate for item in sublist])
         self.probability = np.min(probabilities)
 
-        # rospy.logerr(best_estimate)
+        rospy.logerr(np.array(self.best_estimate) - self.gt[0:7])
+        # rospy.logerr(np.array(self.best_estimate) - np.array(new_joint_positions)[0:7])
         # rospy.logerr(probabilities)
 
     def calibrate(self):
         joint_state_subscriber = Subscriber("/joint_states", JointState)
         gravity_vector_subscriber = Subscriber("blue_hardware/gravity_vectors", GravityVectorArray)
-        tss = ApproximateTimeSynchronizer([joint_state_subscriber, gravity_vector_subscriber], queue_size=2, slop=0.05)
+        tss = ApproximateTimeSynchronizer([joint_state_subscriber, gravity_vector_subscriber], queue_size=50, slop=0.01)
 
         # register joint states and gravity vectors callback
         tss.registerCallback(self.update_filter)
+#
+    # def exit(self):
+        # if self.debug:
+
 
 if __name__ == "__main__":
+
     rospy.init_node("simple_startup_calibration")
 
     # building the kinematic chains
     rospy.loginfo("Building Blue Object...")
     blue = BlueRobotFilter(debug=True)
+    # atexit.register(blue.exit)
 
     # start the calibration callback functions
     blue.calibrate()
 
     # wait until a high probability is reached
     r = rospy.Rate(1.0)
-    while blue.get_probability() < 0.9:
+    start = rospy.get_rostime()
+    while blue.get_probability() < 0.9 and not rospy.is_shutdown() and start + rospy.Duration.from_sec(60) > rospy.get_rostime():
         r.sleep();
 
     # Read startup angles from parameter server
