7a40d0fc4120b2512e74db2abe6ea87dcaf2e760
Brent Yi <yibrenth@gmail.com>
2019-01-28 21:40:53 -0800

    race condition fix (maybe a little hacky?)

--- blue_hardware_interface/src/blue_hardware_interface.cpp
+++ blue_hardware_interface/src/blue_hardware_interface.cpp
@@ -125,8 +125,17 @@ bool BlueHW::jointStartupCalibration(
     blue_msgs::JointStartupCalibration::Request &request,
     blue_msgs::JointStartupCalibration::Response &response
 ) {
+
+  // Create a dummy kinematics object so we can use its transmissions etc
+  // TODO: possibly a little bit hacky?
+  BlueKinematics dummy_kinematics_;
+  dummy_kinematics_.init(
+      params_.joint_names,
+      params_.differential_pairs,
+      params_.gear_ratios);
+
   kinematics_.setJointOffsets(
-      kinematics_.findBestJointOffsets(
+      dummy_kinematics_.findBestJointOffsets(
           request.joint_positions,
           params_.actuator_zeros,
           params_.softstop_min_angles,
