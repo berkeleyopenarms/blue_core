1024d33d58be1c9284a5be18b7e7ddbe7c7031f7
Philipp <phil80301@berkeley.edu>
2019-06-12 11:20:19 -0700

    Clean up error prints and variable names

--- blue_bringup/scripts/gripper_calibration_service.py
+++ blue_bringup/scripts/gripper_calibration_service.py
@@ -12,12 +12,10 @@ from controller_manager_msgs.srv import ListControllers
 from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
 from blue_msgs.msg import MotorState
 from std_msgs.msg import Float64MultiArray
-from control_msgs.msg import (
-    GripperCommandAction,
-    GripperCommandGoal,
-)
+from control_msgs.msg import GripperCommandAction
+from control_msgs.msg import GripperCommandGoal
 
-def get_gripper_state(motor_names):
+def get_gripper_actuator_position():
     motor_state_msg = rospy.wait_for_message("blue_hardware/motor_states", MotorState)
     for i, n in enumerate(motor_state_msg.name):
         if n == motor_names[-1]:
@@ -26,15 +24,10 @@ def get_gripper_state(motor_names):
 def handle_load_controllers():
     unload_list = []
     load_controllers_list = ['blue_controllers/gripper_torque_controller', 'blue_controllers/gripper_controller']
-    loaded_controllers    = list_controllers()
-    rospy.logerr(loaded_controllers.controller)
+    loaded_controllers = list_controllers()
     for controller in load_controllers_list:
         load = True
         for loaded in loaded_controllers.controller:
-            rospy.logerr(loaded.name)
-            rospy.logerr(controller)
-            rospy.logerr(controller)
-
             if loaded.name == controller:
                 # controller already loaded
                 if loaded.state == "running":
@@ -43,7 +36,7 @@ def handle_load_controllers():
                 break
         if load:
             # controller not loaded yet
-            controller_response = load_controller(controller)
+            load_controller(controller)
             unload_list.append(controller)
     return unload_list
 
@@ -65,12 +58,12 @@ def handle_calibration_service(input_request):
             rospy.sleep(0.1)
 
         # Wait until gripper is closed/stops moving
-        gripper_state = get_gripper_state(motor_names)
-        current_position = gripper_state + 10
-        while not rospy.is_shutdown() and np.abs(current_position - gripper_state) > 1e-5:
+        gripper_position_previous = get_gripper_actuator_position()
+        gripper_position_current = gripper_position_previous + 10
+        while not rospy.is_shutdown() and np.abs(gripper_position_current - gripper_position_previous) > 1e-5:
             cmd_pub.publish(cmd)
-            current_position = gripper_state
-            gripper_state = get_gripper_state(motor_names)
+            gripper_position_current = gripper_position_previous
+            gripper_position_previous = get_gripper_actuator_position()
 
         # Stop applying force
         cmd.data = [0.0]
@@ -83,9 +76,9 @@ def handle_calibration_service(input_request):
         response = gripper_position_calibration(1.0)
 
         if response.success:
-            rospy.loginfo("Gripper position calibration succeeded!")
+            rospy.loginfo("Gripper calibration succeeded!")
         else:
-            rospy.logerr("Gripper position calibration failed!")
+            rospy.logerr("Gripper calibration failed!")
 
         # Switch out controllers to reset position
         gripper_controller_response = switch_controller(['blue_controllers/gripper_controller'], ['blue_controllers/gripper_torque_controller'], 2)
@@ -109,7 +102,7 @@ def handle_calibration_service(input_request):
 
         # Clear gripper controllers
         gripper_controller_response = switch_controller([], ['blue_controllers/gripper_controller'], 2)
-        rospy.loginfo("Gripper Calibration Succeeded!")
+        rospy.loginfo("Gripper calibration succeeded!")
 
         # Unload controllers that were loaded
         for controller in unload_list:
@@ -117,21 +110,19 @@ def handle_calibration_service(input_request):
 
 
     except rospy.ServiceException as e:
-        rospy.logerr("Joint startup calibration failed: %s" % e)
+        rospy.logerr("Gripper calibration failed: %s" % e)
         return TriggerResponse(False, "Fail")
 
-    return TriggerResponse(True, "Succsess")
+    return TriggerResponse(True, "Success")
 
 if __name__ == "__main__":
-
     rospy.init_node("simple_startup_calibration")
 
-    # Read startup angles from parameter server
-    rospy.loginfo("Reading desired joint angles...")
+    # Get list of motor names from parameter server
     motor_names = rospy.get_param("blue_hardware/motor_names")
 
-    # Wait for calibration service to come up
-    rospy.loginfo("Waiting for calibration service...")
+    # Wait for calibration service and controller manager service
+    rospy.loginfo("Waiting for calibration service and controller manager...")
     rospy.wait_for_service('blue_hardware/gripper_position_calibration')
     rospy.wait_for_service('controller_manager/switch_controller')
 
@@ -140,7 +131,7 @@ if __name__ == "__main__":
     unload_controller = rospy.ServiceProxy('controller_manager/unload_controller', LoadController)
     list_controllers = rospy.ServiceProxy('controller_manager/list_controllers', ListControllers)
 
-    # gripper calibration procedure, first switch to torque controller
+    # Initialize gripper calibration procedure
     gripper_position_calibration = rospy.ServiceProxy('blue_hardware/gripper_position_calibration', GripperPositionCalibration)
     switch_controller = rospy.ServiceProxy('controller_manager/switch_controller', SwitchController)
     cmd_pub = rospy.Publisher("blue_controllers/gripper_torque_controller/command", Float64MultiArray, queue_size=1)
