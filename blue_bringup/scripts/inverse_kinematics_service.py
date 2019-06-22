1b1786ebc6ceaef8634e8fcdb2b3fec121a27b6d
Philipp <phil80301@berkeley.edu>
2019-06-21 20:29:56 -0700

    Correct bugs, make proper service definition
    * add transform lookup functionality
    * edit node name
    * tested service and working!

--- blue_bringup/scripts/inverse_kinematics_service.py
+++ blue_bringup/scripts/inverse_kinematics_service.py
@@ -11,17 +11,17 @@ from geometry_msgs.msg import PoseStamped
 
 class BlueIK:
     def __init__(self):
-        # load in ROS parameters
+        # load in robot kinematic information
         self.baselink = rospy.get_param("blue_hardware/baselink")
         self.endlink = rospy.get_param("blue_hardware/endlink")
         self.posture_target = rospy.get_param("blue_hardware/posture_target")
         urdf = rospy.get_param("/robot_description")
 
-        # set up tf2
+        # set up tf2 for transformation lookups
         self.tf_buffer = tf2_ros.Buffer(rospy.Duration(10.0)) #tf buffer length
         tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
-        # Build ik solver
+        # Build trac-ik solver
         self.ik = TRAC_IK(self.baselink,
                           self.endlink,
                           urdf,
@@ -33,18 +33,26 @@ class BlueIK:
 
         rospy.Service('inverse_kinematics', InverseKinematics, self.handle_ik_request)
 
-    def ik_solution(self, target, joints):
+    def ik_solution(self, target, joints, solver="trac-ik"):
         # target is a target pose object
         # joints is the starting joint seed
         seed_state = []
         for j in joints:
             seed_state.append(j)
 
-        result = self.ik.CartToJnt(seed_state,
-                                   target.position.x, target.position.y, target.position.z,
-                                   target.orientation.x, target.orientation.y, target.orientation.z, target.orientation.w)
+        solver = solver.lower()
 
-        if not len(result) == self.num_joints:
+        # use the requested solver, default to trac-ik otherwise
+        if solver == "trac-ik":
+            result = self.ik.CartToJnt(seed_state,
+                                       target.position.x, target.position.y, target.position.z,
+                                       target.orientation.x, target.orientation.y, target.orientation.z, target.orientation.w)
+        else:
+            result = self.ik.CartToJnt(seed_state,
+                                       target.position.x, target.position.y, target.position.z,
+                                       target.orientation.x, target.orientation.y, target.orientation.z, target.orientation.w)
+
+        if not len(self.posture_target) == len(result):
             return False
         return result
 
@@ -56,23 +64,21 @@ class BlueIK:
 
         try:
             # apply lookup transformation desired pose and apply the transformation
-            transform = tf_buffer.lookup_transform(self.baselink,
+            transform = self.tf_buffer.lookup_transform(self.baselink,
                                                    request.end_effector_pose.header.frame_id,
                                                    rospy.Time(0), #get the tf at first available time
                                                    rospy.Duration(0.1)) #wait for 0.1 second
             pose_in_baselink_frame = tf2_geometry_msgs.do_transform_pose(request.end_effector_pose, transform)
 
-            if request.solver == "trac-ik":
-                ik_joints = self.ik_solution(pose_in_baselink_frame.pose, seed_joints)
-            else:
-                ik_joints = self.ik_solution(pose_in_baselink_frame.pose, seed_joints)
+            # get the ik solution
+            ik_joints = self.ik_solution(pose_in_baselink_frame.pose, seed_joints)
 
             if ik_joints == False:
-                return InverseKinematicsResponse([], False)
+                return InverseKinematicsResponse(False, [])
             else:
-                return InverseKinematicsResponse(ik_joints, False)
+                return InverseKinematicsResponse(True, ik_joints)
         except:
-            return InverseKinematicsResponse([], False)
+            return InverseKinematicsResponse(False, [])
 
 
 if __name__ == "__main__":
