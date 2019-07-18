11d8f230e89a8c87dceff1ccaa18db39978c53d9
Greg Balke <gbalke@berkeley.edu>
2019-07-18 12:29:23 -0700

    Removing/updating user update math

--- blue_controller_manager/src/blue_controller_manager.cpp
+++ blue_controller_manager/src/blue_controller_manager.cpp
@@ -33,12 +33,9 @@ int main(int argc, char** argv)
   int consecutive_errors = 0;
   int num_errors = 0;
   long count = 0, last_count = 0;
-  double sum_freq = 0.0;
 
   while (ros::ok())
   {
-    ros::Time temp_time = ros::Time::now();
-
     try {
       robot.read();
       consecutive_errors = 0;
@@ -54,22 +51,20 @@ int main(int argc, char** argv)
 
     ros::Time current_time = ros::Time::now();
     cm.update(current_time, current_time - prev_time);
-    double frequency = 1.0 / (current_time - prev_time).toSec();
-    sum_freq += frequency;
     prev_time = current_time;
 
     robot.write();
 
     // Information for user!
-    if ((current_time - last_update).toSec() > 1.0) {
+    double elapsed = (current_time - last_update).toSec();
+    if (elapsed > 1.0) {
       long iters = count - last_count;
       double error_perc = (double)(num_errors) / (double)(iters) * 100.0;
-      double avg_freq = sum_freq / (double)(iters);
+      double avg_freq = (double)(iters) / elapsed;
       ROS_INFO("Communication frequency is %.2fHz with %.2f%% error rate", avg_freq, error_perc);
       last_update = current_time;
       last_count = count;
       num_errors = 0;
-      sum_freq = 0.0;
     }
 
     count++;
